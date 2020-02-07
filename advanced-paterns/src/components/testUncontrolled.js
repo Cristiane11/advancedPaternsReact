cbfda1f6c8f5b75198f74e4188d4d5c33b356ff5
Cristiane11 <bjwmcd@yahoo.com>
2020-02-07 05:53:24 -0500

    update

---
+++ advanced-paterns/src/components/testUncontrolled.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const testUncontrolled =()=>(
+    <form>
+        <input type='text'/>
+        <button>Submit</button>
+    </form>
+)
+ 
+export default testUncontrolled;
