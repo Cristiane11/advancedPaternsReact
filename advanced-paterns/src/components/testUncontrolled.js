3e50c147ff46b901624ea42a2e7ed25f1c2e3369
Abreu <cabreu@carsdirect.win>
2020-02-21 08:09:36 -0500

    update

--- advanced-paterns/src/components/testUncontrolled.js
+++ advanced-paterns/src/components/testUncontrolled.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
-const testUncontrolled =()=>{
+class testUncontrolled extends React.Component{
+
+    constructor(props){
+        super(props)
+        this.handleChange =this.handleChange.bind(this)
+    }
+    handleChange({target}){
+        console.log(target.value)
+    }
+    render(){
     return(
     <form>
-        <input type='text'/>
+        <input type='text' onChange={this.handleChange}/>
         <button>Submit</button>
     </form>
     )
+}
 }
 export default testUncontrolled;
